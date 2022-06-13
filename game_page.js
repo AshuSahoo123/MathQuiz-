726db6260ab8612d645b5b94392952e8c75edf52
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2022-06-13 17:55:23 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -24,4 +24,44 @@ function send() {
 
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value = "";
+    document.getElementById("word1").value = "";
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check() {
+    answer = word * word1;
+    answer1 = document.getElementById("input_check_box").value;
+
+    if (answer == answer1) {
+        if (answer_turn == "player1") {
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else {
+            player2_score = player2_score + 1;
+            document.getElementById("player1_score").innerHTML = player2_score;
+        }
+    }
+
+    if (question_turn == "player1") {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question turn: " + player2_name;
+    }
+    else {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question turn: " + player1_name;
+    }
+
+    if (answer_turn == "player1") {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer turn: "+ player2_name;
+    }
+    else {
+        answer_turn = "player_1";
+        document.getElementById("player_answer").innerHTML = "Answer turn: " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
 }
